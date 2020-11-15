82b2ad17c27cb7f8a3fb7f1dd7a163a6aa7e5f92
Traky <dengk7@mcmaster.ca>
2020-11-15 14:56:16 -0500

    added sound

--- app.js
+++ app.js
@@ -6,14 +6,43 @@ class DrumKit {
         this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
         this.bpm = 150;
+        this.isPlaying = null; // set up to prevent start() function get called while the track is playing
     }
     repeat() {
         let step = this.index % 8;
-        const activeBars = document.querySelectorAll(`b${step}`)
+        const activeBars = document.querySelectorAll(`.b${step}`)
+        activeBars.forEach(bar => {
+            bar.style.animation = "playTrack 0.3s ease-in-out alternate 2"  // style.animationDirection is alternate
+            // play sounds if active
+            if (bar.classList.contains("active")) {
+                if (bar.classList.contains("kick-pad")) {
+                    // set audio currentTime to 0 every time a pad is active 
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                } else if (bar.classList.contains("snare-pad")) {
+                    // set audio currentTime to 0 every time a pad is active 
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                } else if (bar.classList.contains("hihat-pad")) {
+                    // set audio currentTime to 0 every time a pad is active 
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        })
         this.index++;
         console.log(step);
     }
     start() {
+
+        // prevent starting if it's already playing
+        if (this.isPlaying) {
+            return;
+        } else {
+            this.isPlaying = "Playing";
+        }
+
+        // tempo
         const interval = (60 / this.bpm) * 1000;
         setInterval(() => {
             this.repeat();
@@ -32,5 +61,10 @@ document.querySelector("#play-button button").addEventListener("click", () => {
 })
 
 drumKit.pads.forEach(pad => {
-    pad.addEventListener("click", drumKit.activePad)
+    pad.addEventListener("click", drumKit.activePad);
+    pad.addEventListener("animationend", function () {
+        this.style.animation = "";
+        // reset html style tag after animation is done
+        // so that when style tag is updated again, keyframes animation starts
+    });
 })
